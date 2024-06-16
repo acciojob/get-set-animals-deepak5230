be33fb3ad2ab85d278a34e5893580f2f25a94ce9
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2024-06-16 15:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Animal {}
+class Animal {
+	  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound the animal makes
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Define the Cat class that extends Animal
+class Cat extends Animal {
+  // Method to log "purr" to the console
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Define the Dog class that extends Animal
+class Dog extends Animal {
+  // Method to log "woof" to the console
+  bark() {
+    console.log("woof");
+  }
+}
+
+}
+
+//class Dog extends Animal {}
+
+//class Cat extends Animal {}
 
 // Do not change the code below this line
 window.Animal = Animal;
